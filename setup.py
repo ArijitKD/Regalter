5e1aabb67cb5b4bb6f429380d8ffd8f1419a24f9
Arijit Kumar Das <arijitkrdas2004@outlook.com>
2021-05-08 23:01:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,44 @@
+'''
+Regalter: A simple utility for changing the registered owner and
+registered organization names without messing with Windows registry.
+This source code is for the 64-bit version of Regalter, created
+exclusively for running on 64-bit Windows.
+
+This file is a part of Regalter.
+
+Copyright (C) 2021 Arijit Kumar Das (Github: @ArijitKD)
+
+Regalter is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Regalter is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Regalter.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+# This is the setup file which builds the executable for Regalter.
+# Invoke this script as "python setup.py build" (without quotes)
+# in the directory of regalter using a terminal. This setup script
+# must be in the same directory as the other source files in this
+# repository.
+
 import sys
 from cx_Freeze import setup, Executable
 import os
 
-# Dependencies are automatically detected, but it might need fine tuning.
+# Dependencies are automatically detected, but it might need find tuning
 build_exe_options = {"packages": ["tkinter", "ctypes", "os", "sys"], "include_files": ["assets\\"]}
 
-# GUI applications require a different base on Windows (the default is for a
-# console application).
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
-
-script = "main.py"
-#init_script = r"C:\Users\ARIJIT\Desktop\Repos\regalter-utility\reglauncher.py"
-
-## The image and sound files are added manually into the zip file
-## A fix for this would be released
-
 setup  (name = "Regalter (64-bit)",
-       #icon = "assets\\icon.ico",
-        description = "Regalter: GUI modifier for Windows owner and organization (64-bit)",
+        target_name = "regalter_x64",
+        copyright = "Copyright (c) 2021 Arijit Kumar Das.",
+        description = "Regalter: A simple utility for modifying the registered owner and organization names in Windows.",
         options = {"build_exe": build_exe_options},
-        executables = [Executable(script=script, base=base,
+        executables = [Executable(script="main.py", base="Win32GUI",
                                   icon="assets\\icon.ico")])
