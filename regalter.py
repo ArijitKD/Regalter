d0584167a32b4acfb8b69ec12a0db9654f611504
Arijit Kumar Das <arijitkrdas2004@outlook.com>
2021-05-09 12:05:37 +0530

    Update regalter.py

--- regalter.py
+++ regalter.py
@@ -219,18 +219,18 @@ Regalter: A simple utility for changing the registered owner and
 registered organization names without messing with Windows registry.
 Regalter (64-bit) is exclusively for running on 64-bit Windows.
 
-This program is free software: you can redistribute it and/or modify
+Regalter is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
+Regalter is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+along with Regalter. If not, see <https://www.gnu.org/licenses/>.
 
 For any suggestions or for reporting bugs, please send an e-mail to
 arijitkdgit.official@gmail.com.
